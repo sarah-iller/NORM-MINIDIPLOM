6caba012c03e4bc1bff5972eab0d17e21e74d13a
sarah-iller <sarah.iller@mac.com>
2024-12-05 16:38:07 +0100

    Adding Content to the Navigation

--- XX_Plugins/H_BrowserPlugin/content.js
+++ XX_Plugins/H_BrowserPlugin/content.js
@@ -3,8 +3,14 @@ navigationDiv.className = "navigation_left";
 navigationDiv.id = "stickyDiv";
 navigationDiv.textContent = "Plug yourself in.";
 
-// Append the div to the body
 document.body.appendChild(navigationDiv);
+
+const secondDiv = document.createElement("div");
+secondDiv.className = "navigation_content";
+secondDiv.textContent = "This is the second div.";
+
+navigationDiv.appendChild(secondDiv);
+
 
 document.addEventListener("scroll", () => {
     const stickyDiv = document.getElementById("stickyDiv");

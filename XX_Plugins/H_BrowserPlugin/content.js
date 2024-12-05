c91e1b21acb6688c3f75177d865bc7f75e699e35
sarah-iller <sarah.iller@mac.com>
2024-12-05 16:28:38 +0100

    add daily journal entry

--- XX_Plugins/H_BrowserPlugin/content.js
+++ XX_Plugins/H_BrowserPlugin/content.js
@@ -1,6 +1,18 @@
 const navigationDiv = document.createElement("div");
 navigationDiv.className = "navigation_left";
+navigationDiv.id = "stickyDiv";
 navigationDiv.textContent = "Plug yourself in.";
 
 // Append the div to the body
 document.body.appendChild(navigationDiv);
+
+document.addEventListener("scroll", () => {
+    const stickyDiv = document.getElementById("stickyDiv");
+  
+    // Get the current scroll position
+    const scrollTop = window.scrollY; // Distance from the top
+  
+    // Update the position of the sticky div
+    stickyDiv.style.top = `${scrollTop}px`;
+  });
+  

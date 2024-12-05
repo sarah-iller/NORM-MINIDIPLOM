e0199cc19d5962fe94a0a7678b5e97d5014bd21f
sarah-iller <sarah.iller@mac.com>
2024-12-05 16:00:36 +0100

    New Plugin
    Creating a Navigation to interact with the Plugin!

---
+++ XX_Plugins/H_BrowserPlugin/content.js
@@ -0,0 +1,6 @@
+const navigationDiv = document.createElement("div");
+navigationDiv.className = "navigation_left";
+navigationDiv.textContent = "Plug yourself in.";
+
+// Append the div to the body
+document.body.appendChild(navigationDiv);

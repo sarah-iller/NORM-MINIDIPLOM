1b0c73c157a51880fd90537a3daf05c9423af353
sarah-iller <sarah.iller@mac.com>
2024-12-12 16:42:31 +0100

    Work on Website, Add Schematic

---
+++ MiniDpiplom_Website/assets/script/script.js
@@ -0,0 +1,31 @@
+// Select the clickable element and the target element
+const button_intro = document.getElementById("intro");
+const response_intro = document.getElementById("main");
+
+const button_schema = document.getElementById("schema");
+const response_schema = document.getElementById("img01");
+
+// Add a click event listener
+button_intro.addEventListener("click", () => {
+    response_schema.style.filter = "blur(5px)";
+    response_schema.style.width = "40vw";
+    // response_schema.style.opacity = "0.2";
+
+    response_intro.style.filter = "blur(0px)";
+    response_intro.style.opacity = "1";
+
+}
+);
+
+
+button_schema.addEventListener("click", () => {
+    response_intro.style.filter = "blur(5px)";
+    // response_intro.style.opacity = "0.2";
+
+    response_schema.style.filter = "blur(0px)";
+    response_schema.style.opacity = "1";
+    response_schema.style.width = "100vw";
+
+}
+);
+
